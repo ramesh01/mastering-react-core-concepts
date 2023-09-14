5836c01ff4f6baaf621be7d2a3dd4dad2b1eea1f
rameshhb37 <rameshhb37@gmail.com>
2023-09-14 22:57:45 +0530

    Misc: createApp topic added

--- src/util/constants.tsx
+++ src/util/constants.tsx
@@ -6,5 +6,13 @@ export const chaptersList = [
     {
         name: 'Forwarding-ref',
         route: 'forwarding-ref'
+    },
+    {
+        name: 'Props & State',
+        route: 'props-state'
+    },
+    {
+        name: 'React Portal',
+        route: 'protals'
     }
 ];
